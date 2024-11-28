9939cd6a2d762338645cffc0277a0c71fec2f944
Pma10 <115062283+Pma10@users.noreply.github.com>
2024-11-28 21:12:54 +0900

    반응속도 오류 개선

---
+++ script-my.js
@@ -0,0 +1,15 @@
+let playing = false;
+let checkingCounts = 0;
+let latencies = [];
+let isClickable = false;
+let startTime = null;
+let timeoutId = null; 
+
+const mainFrame = document.getElementById("clickContainer");
+const mainFrameText = document.getElementById("clickContainerText");
+
+function loadResults(){
+    console.log(local)
+}
+
+loadResults()
